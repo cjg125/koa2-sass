12e1ab6b207ac89c289fcd6e6723e54e21e7a497
caojungang <caojungang@sogou-inc.com>
2019-06-28 16:07:46 +0800

    format code and upgrade

--- example/index.js
+++ example/index.js
@@ -1,33 +1,39 @@
-'use strict'
+"use strict";
 
-const { resolve } = require('path')
-const Koa = require('koa')
-const app = new Koa()
-const sass = require('../lib')
-app.use(sass({
-  debug: true, // defult: false
-  path: resolve('example/sass'),
-  outPath: resolve('example/css') // defult: null
-}, {
-  includePaths: [resolve('example/sass')]
-}))
+const { resolve } = require("path");
+const Koa = require("koa");
+const app = new Koa();
+const sass = require("../lib");
+app.use(
+  sass(
+    {
+      debug: true, // defult: false
+      path: resolve("example/sass"),
+      outPath: resolve("example/css") // defult: null
+    },
+    {
+      includePaths: [resolve("example/sass")]
+    }
+  )
+);
 
-var tpl = '\
-  <!DOCTYPE html>\
-  <html lang="en">\
-  <head>\
-    <meta charset="UTF-8">\
-    <title></title>\
-    <link rel="stylesheet" href="./index.css">\
-  </head>\
-  <body>\
-    Hello World\
-  </body>\
-  </html>\
-'
+var tpl = `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>koa2-sass</title>
+  </head>
+  <body>
+    <h1>say ~~~ hi</h1>
+  </body>
+  </html>
+`;
 
-app.use(async(ctx, next) => {
-  ctx.body = tpl
-})
+app.use(async (ctx, next) => {
+  ctx.body = tpl;
+});
 
-app.listen(3000)
+app.listen(3000);
